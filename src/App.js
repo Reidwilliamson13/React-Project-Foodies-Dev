3cf7a5284b071eaf5d7c1c9a1509e30f9266480d
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-02 17:27:39 -0500

    Nav button markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./Components/navigation/Navbar";
 import Home from "./Components/static/Home";
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
---
+++ src/Components/recipes/ListRecipes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListRecipes = () => {
+  return (
+    <div>
+      <h1>List Recipes</h1>
+    </div>
+  );
+};
+
+export default ListRecipes;
