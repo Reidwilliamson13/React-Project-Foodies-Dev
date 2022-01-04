5c8b2329d95e260ea1098bfa7e58aa79fa348345
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-04 15:43:14 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./Components/navigation/Navbar";
 import Home from "./Components/static/Home";
+import ListRecipes from "./components/recipes/ListRecipes";
+import NewRecipe from "./Components/recipes/NewRecipe";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -8,7 +10,9 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-      <Route path="/" element{<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<ListRecipes />} />
+        <Route path="/recipes/new" element={<NewRecipe />} />
       </Routes>
     </Router>
   );
